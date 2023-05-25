296feaa715cb0d48827e3ac8e1c25bd94b76be35
Constantin Piber <cp.piber@gmail.com>
2023-05-25 23:24:33 +0200

    Log file upload

--- src/server/api/familie_profile.php
+++ src/server/api/familie_profile.php
@@ -1,6 +1,8 @@
 <?php
 
 function api_getfampicture($msg) {
+  global $conn;
+
   if ( $_SERVER['REQUEST_METHOD'] === 'GET' ) {
     header( 'Content-Type: image/png' );
     $loc = array_key_exists( 'image', $_GET ) ? STATIC_DIR . strval( $_GET['image'] ) : null;
@@ -27,6 +29,16 @@ function api_getfampicture($msg) {
       try {
         if ( !file_exists( STATIC_DIR ) ) mkdir( STATIC_DIR, 0755 );
         move_uploaded_file( $_FILES['image']['tmp_name'], $filepath );
+
+        $sql3 = "INSERT INTO `logs` (`Type`, `Val`) VALUES (?, ?)";
+        $logdata = array();
+        $logdata[] = 'upload';
+        $logdata[] = $filename;
+        if ( !empty( $logdata ) ) {
+          $stmt = $conn->prepare( $sql3 );
+          $stmt->execute( $logdata );
+        }
+
         $msg['status'] = 'success';
         $msg['data'] = $filename;
       } catch ( Exception $e ) {
